3d321a3bab85008597ba5fc4aefdbd3361d75ee4
Benjamin Maggi <bmaggi@salesforce.com>
2021-04-22 16:36:39 -0300

    feat: add nut test

---
+++ test/commands/display.nut.ts
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { expect } from 'chai';
+import { execCmd, TestSession } from '@salesforce/cli-plugins-testkit';
+import { Env } from '@salesforce/kit';
+import { ensureString, getString } from '@salesforce/ts-types';
+
+export interface ListApiDisplayOutput {
+  status: number;
+  result: Array<Record<string, unknown>>;
+}
+
+describe('force:limits:api:display', () => {
+  const env = new Env();
+  let username: string;
+  let testSession: TestSession;
+
+  before('prepare session and ensure environment variables', () => {
+    username = ensureString(env.getString('TESTKIT_HUB_USERNAME'));
+    testSession = TestSession.create({});
+  });
+
+  after(async () => {
+    await testSession?.clean();
+  });
+
+  it('Displays the limits (json)', () => {
+    const output = execCmd<ListApiDisplayOutput>(`force:limits:api:display -u ${username} --json`, {
+      ensureExitCode: 0,
+    }).jsonOutput;
+    expect(output.result).length.greaterThan(0);
+    expect(output.status).to.equal(0);
+  });
+
+  it('should authorize an org using jwt (human readable)', () => {
+    const command = `force:limits:api:display -u ${username}`;
+    const result = execCmd(command, { ensureExitCode: 0 });
+    const output = getString(result, 'shellOutput.stdout');
+    expect(output).to.include('ActiveScratchOrgs');
+  });
+});
